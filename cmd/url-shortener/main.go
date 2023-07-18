23f820dbbebb0a087781b527ae3a9857a6419720
Satzhan Kanatbayev <k.satzhan@gmail.com>
2023-07-18 21:13:20 +0500

    Config made

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
+
+func main() {
+	// TODO: init config: cleanenv
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+	// TODO: init logger: slog
+	// TODO: storage: sqlite
+	// TODO: init router: chi, "chi render"
+	// TODO: run server:
+}
