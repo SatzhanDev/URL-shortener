f001bf012771f8c39dd232193bc5aa3c1b23c40f
Satzhan Kanatbayev <k.satzhan@gmail.com>
2023-07-27 11:34:37 +0500

    added GetURL func

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -33,14 +33,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	id, err := storage.SaveURL("https://translate.google.com/", "translate")
-	if err != nil {
-		log.Error("failed to save url", sl.Err(err))
-		os.Exit(1)
-	}
-
-	log.Info("saved url", slog.Int64("id", id))
-
 	_ = storage
 
 	// TODO: init router: chi, "chi render"
